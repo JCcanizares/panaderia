3efa4435d933f5e5b7c62f9501e23a8b766bd83a
jccanizares <juancarloscanizares@gmail.com>
2016-11-28 17:27:17 +0100

    add t page for chromecast

---
+++ tv.php
@@ -0,0 +1,52 @@
+<?php
+    include 'includes/cabecera.php';
+?>
+<section id="tv_page">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+				<h1>www.panaderiacañizares.es</h1>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-6">
+				<section id="facebook_feed_tv">
+				    <div id="fb-root"></div>
+					<script>(function(d, s, id) {
+					  var js, fjs = d.getElementsByTagName(s)[0];
+					  if (d.getElementById(id)) return;
+					  js = d.createElement(s); js.id = id;
+					  js.src = "//connect.facebook.net/es_ES/sdk.js#xfbml=1&version=v2.8";
+					  fjs.parentNode.insertBefore(js, fjs);
+					}(document, 'script', 'facebook-jssdk'));</script>
+					<div class="fb-page" data-href="https://www.facebook.com/Panader&#xed;a-Ca&#xf1;izares-120333318393832" data-tabs="timeline" data-width="500" data-height="380" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false"><blockquote cite="https://www.facebook.com/Panader&#xed;a-Ca&#xf1;izares-120333318393832" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/Panader&#xed;a-Ca&#xf1;izares-120333318393832">Panadería Cañizares</a></blockquote></div>
+									    
+				</section>
+			</div>
+			<div class="col-md-6">
+                <section id="horario">
+
+                    <div class="row">
+                        <div id="horario_general">
+                            <i class="fa fa-clock-o"></i>
+                            <h2>Nuestro horario</h2>
+                            <h3>Invierno</h3>
+                            <p>Lunes a Sábado de 5:30 a 14:00. Lunes a Viernes de 17:00 a 19:00</p>
+                            <h3>Verano (Julio y Agosto)</h3>
+                            <p>Lunes a Sábado de 5:30 a 14:00. Tardes cerrado.</p>
+                        </div>
+                    </div>
+
+                </section>
+			</div>
+		</div>
+	</div>
+    <?php
+        include 'includes/gallery_2016.php';
+    ?>
+</div>
+
+
+<?php
+    include 'includes/cabecera_fin.php';
+?>
